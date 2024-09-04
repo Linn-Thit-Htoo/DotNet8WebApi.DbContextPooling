9df2d0a1bc442d2095558314f3fb2bd8db797d6e
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 09:26:43 +0630

    -

--- DotNet8WebApi.DbContextPooling/Db/AppDbContext.cs
+++ DotNet8WebApi.DbContextPooling/Db/AppDbContext.cs
@@ -1,14 +1,12 @@
 ﻿using DotNet8WebApi.DbContextPooling.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8WebApi.DbContextPooling.Db
-{
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions options) : base(options)
-        {
-        }
+namespace DotNet8WebApi.DbContextPooling.Db;
 
-        public DbSet<Tbl_Blog> Tbl_Blogs { get; set; }
-    }
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
+
+    public DbSet<Tbl_Blog> Tbl_Blogs { get; set; }
 }
