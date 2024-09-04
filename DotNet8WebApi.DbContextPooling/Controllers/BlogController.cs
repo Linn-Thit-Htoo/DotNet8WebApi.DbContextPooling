2124de029cddbbc118131f32d43fc2933f9e98d4
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-04 09:23:26 +0630

    Add project files.

---
+++ DotNet8WebApi.DbContextPooling/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using DotNet8WebApi.DbContextPooling.Db;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8WebApi.DbContextPooling.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogs(CancellationToken cancellationToken)
+        {
+            var blogs = await _context.Tbl_Blogs.OrderByDescending(x => x.BlogId).ToListAsync(cancellationToken: cancellationToken);
+            return Ok(blogs);
+        }
+    }
+}
---
+++ DotNet8WebApi.DbContextPooling/Db/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DotNet8WebApi.DbContextPooling.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8WebApi.DbContextPooling.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Tbl_Blog> Tbl_Blogs { get; set; }
+    }
+}
